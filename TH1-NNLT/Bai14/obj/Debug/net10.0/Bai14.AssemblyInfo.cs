8ffdd98516c88aa0b51fe00fd885468be848c84b
justhuy23 <luxubu399@gmai.com>
2026-09-25 16:53:03 +0700

    bibilabu

--- TH1-NNLT/Bai14/obj/Debug/net10.0/Bai14.AssemblyInfo.cs
+++ TH1-NNLT/Bai14/obj/Debug/net10.0/Bai14.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai14")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cd66f1e20f2875a51331155365e2ad75fc83851")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b095f84f3d875dd1ecbe010f4025a73318ec26aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai14")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai14")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
